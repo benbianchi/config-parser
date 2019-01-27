d53855cffc193a836888c8c910cee97b86d88367
Ben Bianchi <bgb319@gmail.com>
2019-01-27 17:08:53 -0500

    initial commit

---
+++ config-parser/src/main/java/com/bbianchi/view/View.java
@@ -0,0 +1,56 @@
+package com.bbianchi.view;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class View implements Serializable {
+    
+    @JsonProperty
+    private String identifier= "";
+
+    @JsonProperty("class")
+    private String classField = "";
+
+    @JsonProperty("classNames")
+    private List<String> classNames;
+
+    @JsonProperty("subviews")
+    private List<View> subviews;
+
+
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    public void setIdentifier(String newIdentifier) {
+        this.identifier = newIdentifier;
+    }
+
+    public String getClassField() {
+        return this.classField;
+    }
+
+    public void setClassField(String newClassField) {
+        this.classField = newClassField;
+    }
+    
+    public List<String> getClassNames() {
+        return this.classNames;
+    }
+
+    public void setClassNames(List<String> newClassNames) {
+        this.classNames = newClassNames;
+    }
+   
+    public List<View> getSubviews() {
+        return this.subviews;
+    }
+
+    public void setSubViews(List<View> newSubviews) {
+        this.subviews = newSubviews;
+    }
+}
---
+++ config-parser/src/test/java/com/bbianchi/view/ViewTest.java
@@ -0,0 +1,78 @@
+package com.bbianchi.view;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Test;
+
+public class ViewTest {
+
+    private Object deserialize(String s, Class clazz) {
+        try {
+            return jsonObjectMapper.readValue(s, clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        return null;
+    }
+
+    ObjectMapper jsonObjectMapper = new ObjectMapper();
+
+    @Test
+    public void viewHasIdentifierField() {
+
+        String viewAsString = "{\"identifier\":\"me\"}";
+        View view = (View) deserialize(viewAsString, View.class);
+        assertEquals(view.getIdentifier().equals("me"), true);
+    }
+
+    @Test
+    public void viewHasClassField() {
+        String viewAsString = "{\"identifier\":\"me\", \"class\":\"StackView\"}";
+        View view = (View) deserialize(viewAsString, View.class);
+
+        assertEquals(view.getClassField().equals("StackView"), true);
+    }
+
+    @Test
+    public void viewHasClassNameField() {
+        String viewAsString = "{\"identifier\":\"me\", \"class\":\"StackView\", \"classNames\":[\"container\"]}";
+        View view = (View) deserialize(viewAsString, View.class);
+
+        assertEquals(view.getClassNames().get(0).equals("container"), true);
+    }
+
+    @Test
+    public void viewHasSubViews() {
+        String subviewAsString = "{\"identifier\":\"childIdentifier\", \"class\":\"ChildStackView\", \"classNames\":[\"child\"]}";
+        String viewAsString = String.format(
+                "{\"identifier\":\"me\", \"class\":\"StackView\", \"classNames\":[\"container\"], \"subviews\":[%s]}",
+                subviewAsString);
+        View view = (View) deserialize(viewAsString, View.class);
+
+        assertEquals(view.getSubviews().size(), 1);
+
+        assertEquals(view.getSubviews().get(0).getClassField().equals("ChildStackView"), true);
+        assertEquals(view.getSubviews().get(0).getClassNames().get(0).equals("child"), true);
+        assertEquals(view.getSubviews().get(0).getIdentifier().equals("childIdentifier"), true);
+    }
+
+    @Test
+    public void canSerializeFixture() {
+        try {
+            jsonObjectMapper.readValue(new File("./src/test/resources/configs/settings-config.json"), View.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+        assertTrue(true);
+    }
+
+}
